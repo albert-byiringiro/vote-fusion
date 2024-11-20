65a8603400f2309fc40ee4fcbf588be603835c80
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-20 12:35:43 +0200

    feature: add service methods

--- server/src/polls/polls.service.ts
+++ server/src/polls/polls.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { CreatePollFields, JoinPollFields, RejoinPollFields } from "./types";
 
 @Injectable()
 export class PollsService {
+    async createPoll(fields: CreatePollFields) {}
+    
+    async joinPoll(fields: JoinPollFields) {}
+    
+    async rejoinPoll(fields: RejoinPollFields) {}
     
 }
