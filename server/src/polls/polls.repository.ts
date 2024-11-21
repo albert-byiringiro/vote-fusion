377c625dd307305c3b023cf43a7837762528c825
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-21 19:15:32 +0200

    feature: add create poll repository method

--- server/src/polls/polls.repository.ts
+++ server/src/polls/polls.repository.ts
@@ -1,6 +1,10 @@
-import { Inject, Injectable, Logger } from "@nestjs/common";
+import { Inject, Injectable, InternalServerErrorException, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import IORedis, { Redis } from "ioredis";
+
+import { Poll } from "shared"
+import { CreatePollData } from "./types";
+
 
 @Injectable()
 export class PollsRepository {
@@ -13,5 +17,42 @@ export class PollsRepository {
         @Inject(IORedis) private readonly redisClient: Redis
     ){
         this.ttl = configService.get('POLL_DURATION')
+    }
+
+    async createPoll({
+        votesPerVoter,
+        topic,
+        pollID,
+        userID,
+    }: CreatePollData): Promise<Poll> {
+        const initialPoll = {
+            id: pollID,
+            topic,
+            votesPerVoter,
+            participants: {},
+            adminID: userID
+        };
+
+        this.logger.log(
+            `Creating new poll: ${JSON.stringify(initialPoll, null, 2)} with TTL ${this.ttl}`
+        )
+
+        const key = `polls:${pollID}`
+
+        try {
+            await this.redisClient
+                .multi([
+                    ['send_command', 'JSON_SET', key, '.', JSON.stringify(initialPoll)],
+                    ['expire', key, this.ttl]
+                ]).exec()
+            
+            return initialPoll;
+        } catch (e) {
+            this.logger.error(
+                `Failed to add poll ${JSON.stringify(initialPoll)}\n${e}`,
+            )
+
+            throw new InternalServerErrorException();
+        }
     }
 }
