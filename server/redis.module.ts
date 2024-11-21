319cd5e44e946747aa73183f10e37f8feb5ef97d
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-21 06:11:42 +0200

    feature: define our created module in the return's providers array

--- server/redis.module.ts
+++ server/redis.module.ts
@@ -1,5 +1,8 @@
 import { DynamicModule, FactoryProvider, Module, ModuleMetadata } from "@nestjs/common";
+import IORedis from "ioredis";
 import Redis, { RedisOptions } from "ioredis";
+
+export const IORedisKey = 'IORedis'
 
 type RedisModuleOptions = {
     connectionOptions: RedisOptions;
@@ -17,11 +20,23 @@ export class RedisModule {
         imports,
         inject
     }: RedisAsyncMOduleOptions): Promise<DynamicModule> {
+        const redisProvider = {
+            provide: IORedisKey,
+            useFactory: async (...args) => {
+                const { connectionOptions, onClientReady } = await useFactory(...args)
+
+                const client = await new IORedis(connectionOptions)
+
+                onClientReady(client)
+            },
+            inject
+        }
+        
         return {
             module: RedisModule,
             imports: imports,
-            providers: [],
-            exports: []
+            providers: [redisProvider],
+            exports: [redisProvider]
         }
     }
 }
