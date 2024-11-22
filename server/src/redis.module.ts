13ed8240c3b3489dc2c1785f1ec1cdf02244c9a8
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-22 17:20:26 +0200

    feature: modify which redis package

--- server/src/redis.module.ts
+++ server/src/redis.module.ts
@@ -1,42 +1,47 @@
-import { DynamicModule, FactoryProvider, Module, ModuleMetadata } from "@nestjs/common";
-import IORedis from "ioredis";
-import Redis, { RedisOptions } from "ioredis";
+import { DynamicModule, FactoryProvider, ModuleMetadata } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import IORedis, { Redis, RedisOptions } from 'ioredis';
 
-export const IORedisKey = 'IORedis'
+export const IORedisKey = 'IORedis';
 
 type RedisModuleOptions = {
-    connectionOptions: RedisOptions;
-    onClientReady?: (client: Redis) => void
-}
+  connectionOptions: RedisOptions;
+  onClientReady?: (client: Redis) => void;
+};
 
-type RedisAsyncMOduleOptions = {
-    useFactory: (...args: any[]) => Promise<RedisModuleOptions> | RedisModuleOptions
-} & Pick<ModuleMetadata, 'imports'> & Pick<FactoryProvider, 'inject'>;
+type RedisAsyncModuleOptions = {
+  useFactory: (
+    ...args: any[]
+  ) => Promise<RedisModuleOptions> | RedisModuleOptions;
+} & Pick<ModuleMetadata, 'imports'> &
+  Pick<FactoryProvider, 'inject'>;
 
 @Module({})
 export class RedisModule {
-    static async registerAsync({
-        useFactory,
-        imports,
-        inject
-    }: RedisAsyncMOduleOptions): Promise<DynamicModule> {
-        const redisProvider = {
-            provide: IORedisKey,
-            useFactory: async (...args) => {
-                const { connectionOptions, onClientReady } = await useFactory(...args)
+  static async registerAsync({
+    useFactory,
+    imports,
+    inject,
+  }: RedisAsyncModuleOptions): Promise<DynamicModule> {
+    const redisProvider = {
+      provide: IORedisKey,
+      useFactory: async (...args) => {
+        const { connectionOptions, onClientReady } = await useFactory(...args);
 
-                const client = await new IORedis(connectionOptions)
+        const client = await new IORedis(connectionOptions);
 
-                onClientReady(client)
-            },
-            inject
-        }
-        
-        return {
-            module: RedisModule,
-            imports: imports,
-            providers: [redisProvider],
-            exports: [redisProvider]
-        }
-    }
+        onClientReady(client);
+
+        return client;
+      },
+      inject,
+    };
+
+    return {
+      module: RedisModule,
+      imports,
+      providers: [redisProvider],
+      exports: [redisProvider],
+    };
+  }
 }
