a42f39b0502234e7d35b88e4ed0836eeeef1ad10
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-22 17:24:28 +0200

    refactor: adjust polls repository

--- server/src/polls/polls.repository.ts
+++ server/src/polls/polls.repository.ts
@@ -20,41 +20,41 @@ export class PollsRepository {
   }
 
 
-    async createPoll({
-        votesPerVoter,
+async createPoll({
+    votesPerVoter,
+    topic,
+    pollID,
+    userID,
+    }: CreatePollData): Promise<Poll> {
+    const initialPoll = {
+        id: pollID,
         topic,
-        pollID,
-        userID,
-      }: CreatePollData): Promise<Poll> {
-        const initialPoll = {
-          id: pollID,
-          topic,
-          votesPerVoter,
-          participants: {},
-          adminID: userID,
-        };
-    
-        this.logger.log(
-          `Creating new poll: ${JSON.stringify(initialPoll, null, 2)} with TTL ${
-            this.ttl
-          }`,
+        votesPerVoter,
+        participants: {},
+        adminID: userID,
+    };
+
+    this.logger.log(
+        `Creating new poll: ${JSON.stringify(initialPoll, null, 2)} with TTL ${
+        this.ttl
+        }`,
+    );
+
+    const key = `polls:${pollID}`;
+
+    try {
+        await this.redisClient
+        .multi([
+            ['send_command', 'JSON.SET', key, '.', JSON.stringify(initialPoll)],
+            ['expire', key, this.ttl],
+        ])
+        .exec();
+        return initialPoll;
+    } catch (e) {
+        this.logger.error(
+        `Failed to add poll ${JSON.stringify(initialPoll)}\n${e}`,
         );
-    
-        const key = `polls:${pollID}`;
-    
-        try {
-          await this.redisClient
-            .multi([
-              ['send_command', 'JSON.SET', key, '.', JSON.stringify(initialPoll)],
-              ['expire', key, this.ttl],
-            ])
-            .exec();
-          return initialPoll;
-        } catch (e) {
-          this.logger.error(
-            `Failed to add poll ${JSON.stringify(initialPoll)}\n${e}`,
-          );
-          throw new InternalServerErrorException();
-        }
-      }
+        throw new InternalServerErrorException();
+    }
+    }
 }
