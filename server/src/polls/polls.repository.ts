76d71c0724ad01647414bf677524ddaa85f2edd8
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-22 17:23:01 +0200

    feature: refactor polls repository

--- server/src/polls/polls.repository.ts
+++ server/src/polls/polls.repository.ts
@@ -1,58 +1,60 @@
-import { Inject, Injectable, InternalServerErrorException, Logger } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import IORedis, { Redis } from "ioredis";
-
-import { Poll } from "shared"
-import { CreatePollData } from "./types";
-
+import { Inject, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Redis } from 'ioredis';
+import { IORedisKey } from 'src/redis.module';
+import { CreatePollData } from './types';
+import { Poll } from 'shared';
 
 @Injectable()
 export class PollsRepository {
-    // to use time-to-live from configuration
-    private readonly ttl: string
-    private readonly logger = new Logger(PollsRepository.name)
+  // to use time-to-live from configuration
+  private readonly ttl: string;
+  private readonly logger = new Logger(PollsRepository.name);
 
-    constructor(
-        configService: ConfigService,
-        @Inject(IORedis) private readonly redisClient: Redis
-    ){
-        this.ttl = configService.get('POLL_DURATION')
-    }
+  constructor(
+    configService: ConfigService,
+    @Inject(IORedisKey) private readonly redisClient: Redis,
+  ) {
+    this.ttl = configService.get('POLL_DURATION');
+  }
+
 
     async createPoll({
         votesPerVoter,
         topic,
         pollID,
         userID,
-    }: CreatePollData): Promise<Poll> {
+      }: CreatePollData): Promise<Poll> {
         const initialPoll = {
-            id: pollID,
-            topic,
-            votesPerVoter,
-            participants: {},
-            adminID: userID
+          id: pollID,
+          topic,
+          votesPerVoter,
+          participants: {},
+          adminID: userID,
         };
-
+    
         this.logger.log(
-            `Creating new poll: ${JSON.stringify(initialPoll, null, 2)} with TTL ${this.ttl}`
-        )
-
-        const key = `polls:${pollID}`
-
+          `Creating new poll: ${JSON.stringify(initialPoll, null, 2)} with TTL ${
+            this.ttl
+          }`,
+        );
+    
+        const key = `polls:${pollID}`;
+    
         try {
-            await this.redisClient
-                .multi([
-                    ['send_command', 'JSON_SET', key, '.', JSON.stringify(initialPoll)],
-                    ['expire', key, this.ttl]
-                ]).exec()
-            
-            return initialPoll;
+          await this.redisClient
+            .multi([
+              ['send_command', 'JSON.SET', key, '.', JSON.stringify(initialPoll)],
+              ['expire', key, this.ttl],
+            ])
+            .exec();
+          return initialPoll;
         } catch (e) {
-            this.logger.error(
-                `Failed to add poll ${JSON.stringify(initialPoll)}\n${e}`,
-            )
-
-            throw new InternalServerErrorException();
+          this.logger.error(
+            `Failed to add poll ${JSON.stringify(initialPoll)}\n${e}`,
+          );
+          throw new InternalServerErrorException();
         }
-    }
+      }
 }
