3c2f1d9645b7fe87ee9baabd85f481da0ef04a22
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-22 17:21:58 +0200

    refactor: adjust polls module

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -1,13 +1,13 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { PollsController } from "./polls.controller";
-import { PollsService } from "./polls.service";
-import { redisModule } from "src/modules.config";
-import { PollsRepository } from "./polls.repository";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { redisModule } from 'src/modules.config';
+import { PollsRepository } from './polls.repository';
+import { PollsService } from './polls.service';
+import { PollsController } from './polls.controller';
 
 @Module({
-    imports: [ConfigModule, redisModule],
-    controllers: [PollsController],
-    providers: [PollsService, PollsRepository]
+  imports: [ConfigModule, redisModule],
+  controllers: [PollsController],
+  providers: [PollsService, PollsRepository],
 })
-export class PollsModule{}
+export class PollsModule {}
