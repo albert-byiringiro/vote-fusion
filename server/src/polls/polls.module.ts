9ef3a38641779cf9778c65015875be2f4d261723
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-24 09:02:49 +0200

    feature: register jwt module in polls module

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { redisModule } from 'src/modules.config';
+import { jwtModule, redisModule } from 'src/modules.config';
 import { PollsRepository } from './polls.repository';
 import { PollsService } from './polls.service';
 import { PollsController } from './polls.controller';
 
 @Module({
-  imports: [ConfigModule, redisModule],
+  imports: [ConfigModule, redisModule, jwtModule],
   controllers: [PollsController],
   providers: [PollsService, PollsRepository],
 })
