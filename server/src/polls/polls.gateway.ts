5fee19daee202393c931f86bb04ee19f5a8b56f1
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-24 12:21:58 +0200

    refactor: remove web socket gateway decorator from polls gateway

--- server/src/polls/polls.gateway.ts
+++ server/src/polls/polls.gateway.ts
@@ -1,16 +1,7 @@
 import { Logger } from "@nestjs/common";
-import { OnGatewayInit, WebSocketGateway } from "@nestjs/websockets";
+import { OnGatewayInit } from "@nestjs/websockets";
 import { PollsService } from "./polls.service";
 
-@WebSocketGateway({
-    namespace: 'polls',
-    cors: {
-        origin: [
-            'http://localhost:8080',
-            /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
-        ],
-    }
-})
 export class PollsGateway implements OnGatewayInit {
     private readonly logger = new Logger(PollsGateway.name)
 
@@ -18,6 +9,6 @@ export class PollsGateway implements OnGatewayInit {
 
     // Gateway initialized (provided in module and instantiated)
     afterInit(): void {
-        this.logger.log(`Websocked Gateway initialized.`)
+        this.logger.log(`Websocket Gateway initialized.`)
     }
 }
