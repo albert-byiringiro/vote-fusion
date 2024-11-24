2896a79381ddb508bd97876b73f30fbbfae6761e
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-24 11:55:00 +0200

    feature: add poll websocket gateway to polls module

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -4,10 +4,11 @@ import { jwtModule, redisModule } from 'src/modules.config';
 import { PollsRepository } from './polls.repository';
 import { PollsService } from './polls.service';
 import { PollsController } from './polls.controller';
+import { PollsGateway } from './polls.gateway';
 
 @Module({
   imports: [ConfigModule, redisModule, jwtModule],
   controllers: [PollsController],
-  providers: [PollsService, PollsRepository],
+  providers: [PollsService, PollsRepository, PollsGateway],
 })
 export class PollsModule {}
