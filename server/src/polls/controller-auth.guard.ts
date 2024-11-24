321d2e1a58249b1d14a750b9f0047225d5736486
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-24 10:02:20 +0200

    refactor: get the context of the request in the auth guard

--- server/src/polls/controller-auth.guard.ts
+++ server/src/polls/controller-auth.guard.ts
@@ -1,8 +1,17 @@
-import { CanActivate, Injectable, Logger } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, Logger } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class ControllerAuthGuard implements CanActivate {
     private readonly logger = new Logger(ControllerAuthGuard.name)
     constructor(private readonly jwtService: JwtService){}
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest();
+
+        this.logger.debug(`Checking for auth token on request body`, request.body)
+
+        return false;
+    }
 }
