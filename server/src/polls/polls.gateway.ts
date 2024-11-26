f7ee2070135435575d8cf26b02763fa0974bac8b
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-11-26 22:02:54 +0200

    feature: added socketWithAuth (contains userID and pollID)

--- server/src/polls/polls.gateway.ts
+++ server/src/polls/polls.gateway.ts
@@ -2,6 +2,7 @@ import { Logger } from "@nestjs/common";
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { PollsService } from "./polls.service";
 import { Namespace, Socket } from "socket.io";
+import { SocketWithAuth } from "./types";
 
 @WebSocketGateway({
     namespace: 'polls',
@@ -18,20 +19,21 @@ export class PollsGateway implements OnGatewayInit, OnGatewayConnection, OnGatew
         this.logger.log(`Websocket Gateway initialized.`)
     }
 
+    handleConnection(client: SocketWithAuth) {
+        const sockets = this.io.sockets
+
+        this.logger.debug(`Socket connected with userID: ${client.userID}, pollID: ${client.pollID}, and name: "${client.name}`)
+
+        this.logger.log(`WS Client with id: ${client.id} connected!`);
+        this.logger.debug(`Number of connected sockets: ${sockets.size}`)
+
+        // TODO - remove client from poll and send `participants_updated` event to remaining clients
+    }
+
     handleDisconnect(client: Socket) {
         const socket = this.io.sockets;
 
         this.logger.log(`WS Client with id: ${client.id} connected!`)
         this.logger.debug(`Number of connected sockets: ${socket.size}`)
-    }
-
-    // TODO - use socketWithAuth (contains userID and pollID)
-    handleConnection(client: Socket) {
-        const sockets = this.io.sockets
-
-        this.logger.log(`Disconnected socket id: ${client.id}`);
-        this.logger.debug(`Number of connected sockets: ${sockets.size}`)
-
-        // TODO - remove client from poll and send `participants_updated` event to remaining clients
     }
 }
