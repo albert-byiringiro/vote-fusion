7da7965fa8bbd99b371f8e15df9137767a93e237
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-01 13:15:13 +0200

    feature: add nomination types to the poll types

--- shared/poll-types.ts
+++ shared/poll-types.ts
@@ -2,11 +2,21 @@ export interface Participants {
     [participantID: string]: string
 }
 
+export type Nomination = {
+    userID: string;
+    text: string
+}
+
+export type Nominations = {
+    [nominationID: string]: Nomination
+}
+
 export interface Poll {
     id: string;
     topic: string;
     votesPerVoter: number;
     participants: Participants;
     adminID: string;
+    nominations: Nominations;
     hasStarted: boolean;
 }
