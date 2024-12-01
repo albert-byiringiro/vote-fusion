5ede37e68b8a8d41c4f92524a4c7e864d83753d4
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-01 17:22:18 +0200

    refactor: modified polls reppsitory code

--- server/src/polls/polls.repository.ts
+++ server/src/polls/polls.repository.ts
@@ -3,7 +3,12 @@ import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Redis } from 'ioredis';
 import { IORedisKey } from 'src/redis.module';
-import { AddNominationData, AddParticipantData, AddParticipantRankingData, CreatePollData } from './types';
+import {
+  AddNominationData,
+  AddParticipantData,
+  AddParticipantRankingsData,
+  CreatePollData,
+} from './types';
 import { Poll } from 'shared';
 
 @Injectable()
@@ -192,35 +197,57 @@ export class PollsRepository {
   }
 
   async startPoll(pollID: string): Promise<Poll> {
-    this.logger.log(`Setting hasStarted for poll: ${pollID}`)
+    this.logger.log(`setting hasStarted for poll: ${pollID}`);
 
-    const key = `polls:${pollID}`
+    const key = `polls:${pollID}`;
 
     try {
-      await this.redisClient.send_command('JSON_SET', key, '.hasStarted', JSON.stringify(true))
+      await this.redisClient.send_command(
+        'JSON.SET',
+        key,
+        '.hasStarted',
+        JSON.stringify(true),
+      );
 
-      return this.getPoll(pollID)
-    } catch (error) {
-      this.logger.error(`Failed set hasStarted for pollID: ${pollID}`, error)
-
-      throw new InternalServerErrorException('There was an error starting the poll.')
+      return this.getPoll(pollID);
+    } catch (e) {
+      this.logger.error(`Failed set hasStarted for poll: ${pollID}`, e);
+      throw new InternalServerErrorException(
+        'The was an error starting the poll',
+      );
     }
   }
 
-  async addParticipantRankings({ pollID, userID, rankings }: AddParticipantRankingData): Promise<Poll> {
-    this.logger.log(`Attempting to add rankings for userID/name: ${userID} to pollID: ${pollID}`, rankings)
+  async addParticipantRankings({
+    pollID,
+    userID,
+    rankings,
+  }: AddParticipantRankingsData): Promise<Poll> {
+    this.logger.log(
+      `Attempting to add rankings for userID/name: ${userID} to pollID: ${pollID}`,
+      rankings,
+    );
 
-    const key = `polls:${pollID}`
-    const rankingsPath = `.rankings.${userID}`
+    const key = `polls:${pollID}`;
+    const rankingsPath = `.rankings.${userID}`;
 
     try {
-      await this.redisClient.send_command('JSON_SET', key, rankingsPath, JSON.stringify(rankings))
+      await this.redisClient.send_command(
+        'JSON.SET',
+        key,
+        rankingsPath,
+        JSON.stringify(rankings),
+      );
 
-      return this.getPoll(pollID)
-    } catch (error) {
-      this.logger.error(`Failed to add a ranking for userID/name: ${userID}/ to pollID: ${pollID}`, rankings)
-
-      throw new InternalServerErrorException('There was an error starting the poll.')
+      return this.getPoll(pollID);
+    } catch (e) {
+      this.logger.error(
+        `Failed to add a rankings for userID/name: ${userID}/ to pollID: ${pollID}`,
+        rankings,
+      );
+      throw new InternalServerErrorException(
+        'There was an error starting the poll',
+      );
     }
   }
 }
