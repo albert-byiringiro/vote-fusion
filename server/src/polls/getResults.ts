2a6ff80be18263b4cc1facccd43cc52f52769730
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-02 08:53:14 +0200

    refactor: added logic for calculating results

--- server/src/polls/getResults.ts
+++ server/src/polls/getResults.ts
@@ -5,5 +5,31 @@ export default (
     nominations: Nominations,
     votesPerVoter: number
 ): Results => {
-    return [];
+    /*
+    1. Each value of of `ranking` key values is an array of a participant's votes. Points for each array element corresponds to following formula:
+    r_n = ((votesPerVoter - 0.5 * n) / votesPerVoter) ^ (n + 1), where n corresponds to array index of rankings.
+    */
+
+    // Accumulate score per nominationID
+    const scores: { [nominationID: string]: number } = {}
+
+    Object.values(rankings).forEach((userRankings) => {
+        userRankings.forEach((nominationID, n) => {
+            const voteValue = Math.pow((votesPerVoter - 0.5 * n) / votesPerVoter, n + 1)
+
+            scores[nominationID] = (scores[nominationID] ?? 0) + voteValue
+        })
+    })
+
+    // 2. Take nominationID to score mapping, and merge in nominationText and nominationID into value
+    const results = Object.entries(scores).map(([nominationID, score]) => ({
+        nominationID,
+        nominationText: nominations[nominationID].text,
+        score
+    }))
+
+    // 3. Sort values by score in descending order
+    results.sort((res1, res2) => res1.score - res2.score)
+
+    return results
 }
