f44efff4af6c445ec6a3e03f94504580e8618982
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-02 07:57:06 +0200

    feature: add getResult computation helper function

---
+++ server/src/polls/getResults.ts
@@ -0,0 +1,9 @@
+import { Nominations, Rankings, Results } from "shared";
+
+export default (
+    rankings: Rankings,
+    nominations: Nominations,
+    votesPerVoter: number
+): Results => {
+    return [];
+}
