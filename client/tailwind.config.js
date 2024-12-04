535b24abb6b09f213d6d5e3c3f9703765378041d
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-04 07:03:23 +0200

    feature: configure tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: colors.orange,
+        purple: colors.violet,
+      },
+    },
   },
   plugins: [],
 }
-
