87fa3a844acc5862de7ff6795d7b88c1a4605534
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-05 09:39:14 +0200

    feature: add count selector component's decrease button

--- client/src/components/ui/CountSelector.tsx
+++ client/src/components/ui/CountSelector.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 type CountSelectorProps = {
     min: number;
     max: number;
@@ -5,3 +7,40 @@ type CountSelectorProps = {
     initial: number;
     onChange: (val: number) => void
 }
+
+const CountSelector: React.FC<CountSelectorProps> = ({
+    min,
+    max,
+    step,
+    initial,
+    onChange
+}) => {
+    if (initial < min || initial > max) {
+        console.warn(`'initial' = ${initial} must be in the range of ${min} and ${max}. Setting a default inital value`)
+
+        const steps = (max - min) / step
+
+        initial = min + Math.floor(steps)
+    }
+
+    const [current, setCurrent] = useState(initial)
+
+    useEffect(() => {
+        onChange(current);
+    }, [current]);
+
+    return (
+        <div className="flex justify-between items-center">
+            <button 
+            type="button" 
+            className="btn-round btn-round-orange"
+            disabled={current - step < min}
+            onClick={() => setCurrent(current - step)}
+            >
+                -
+            </button>
+        </div>
+    )
+}
+
+export default CountSelector
