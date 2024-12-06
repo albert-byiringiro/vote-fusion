25b9cd1d7ef3a8e05ef167208c16a59362e0123b
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-06 02:44:54 +0200

    refactor: applied the render method to each story

--- client/src/stories/BottomSheet.stories.tsx
+++ client/src/stories/BottomSheet.stories.tsx
@@ -1,46 +1,65 @@
-import { Meta, StoryObj } from "@storybook/react/*";
+import { Meta, StoryObj } from "@storybook/react";
 import BottomSheet from "../components/ui/BottomSheet";
 import { ComponentProps } from "react";
 
 type StoryProps = ComponentProps<typeof BottomSheet>
 
 export default {
-    title: 'BottomSheet',
-    component: BottomSheet,
-    argTypes: {
-        onClose: {
-            action: 'closing'
-        }
-    },
-    args: {
-        isOpen: true
+  title: 'BottomSheet',
+  component: BottomSheet,
+  argTypes: {
+    onClose: {
+      action: 'closing'
     }
+  },
+  args: {
+    isOpen: true
+  }
 } as Meta<typeof BottomSheet>
 
 type Story = StoryObj<StoryProps>
 
-export const Default: Story = {
-    render: (args) => {
-        return <BottomSheet {...args} />
-    }
-}
-
 export const OpenBottomSheetShort: Story = {
-    args: {
-        children: (
-            <div className="p-12">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nemo expedita laudantium. Aspernatur reprehenderit maiores exercitationem tempora magnam, quisquam, ut obcaecati corporis odio rerum temporibus hic? Laudantium sed corrupti id?
-            </div>
-        )
-    }
+  render: (args) => (
+    <div className="max-w-sm m-auto h-screen relative">
+      <BottomSheet {...args} />
+    </div>
+  ),
+  args: {
+    children: (
+      <div className="p-12">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis, facere
+        quia enim ut, ad molestiae natus illum rem harum suscipit consectetur
+        voluptatem incidunt. Id adipisci eius architecto vitae, rem quia cum velit
+        totam deleniti repudiandae asperiores, et delectus tempore necessitatibus
+        beatae aspernatur quasi sapiente, labore reiciendis nemo? Hic, eveniet
+        officiis!
+      </div>
+    )
+  }
 }
 
 export const OpenBottomSheetLong: Story = {
-    args: {
-        children: (
-            <div className="p-12">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem animi, laudantium a tempora mollitia reprehenderit corrupti, iusto quasi asperiores eveniet suscipit, architecto quis nisi exercitationem culpa ullam possimus rerum? Beatae, molestiae possimus quo magnam amet dignissimos officia velit ratione animi ullam recusandae necessitatibus, eveniet doloribus blanditiis a expedita. Nisi recusandae ad eum odit veritatis, odio quae quia autem quaerat rerum culpa voluptate incidunt accusamus sequi, ipsum ipsa praesentium quos esse earum. Inventore adipisci libero, enim aperiam ex, tenetur debitis ducimus itaque laboriosam mollitia architecto corrupti praesentium veniam modi distinctio obcaecati animi eius a sunt voluptas. Eos, optio? Minima, quas itaque! Eligendi aspernatur exercitationem aut ducimus suscipit ipsum sit, dolor enim. Culpa fuga incidunt nobis dolorem temporibus ad corporis ea? Quasi?
-            </div>
-        )
-    }
+  render: (args) => (
+    <div className="max-w-sm m-auto h-screen relative">
+      <BottomSheet {...args} />
+    </div>
+  ),
+  args: {
+    children: (
+      <div className="p-12">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste soluta
+        voluptates, unde, at assumenda fugit fuga molestias earum architecto non,
+        nostrum placeat harum accusantium nisi odit amet vel natus est dolores
+        quidem commodi? Praesentium, expedita ratione! Cumque non quod odit
+        laudantium tempora laborum nesciunt iure vel, consequatur voluptate ipsam
+        vero? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui sunt
+        ab maiores perspiciatis eligendi sapiente veniam officiis maxime autem
+        distinctio illo et ut amet earum sit odit error ullam adipisci minima
+        repudiandae, eveniet aliquid dolore necessitatibus! Impedit ea omnis
+        suscipit, magni et pariatur maxime porro, distinctio, ipsam mollitia
+        dolorum officia!
+      </div>
+    )
+  }
 }
