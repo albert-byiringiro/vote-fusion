fef1eff11cf3d89582328382c97148e1f285bffd
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-08 17:31:24 +0200

    feature: added a loader component

--- client/src/components/ui/Loader.tsx
+++ client/src/components/ui/Loader.tsx
@@ -1,3 +1,5 @@
+import classes from './Loader.module.css'
+
 type LoaderProps = {
     color: 'blue' | 'orange' | 'purple';
     isLoading: boolean;
@@ -10,3 +12,27 @@ const colorStyles = {
     purple: 'bg-purple',
 }
 
+const Loader = ({ color, isLoading, width=80 }: LoaderProps) => {
+    const colorStyle = colorStyles[color] || 'bg-purple'
+
+    const d = width / 7;
+
+    const left1 = d
+    const left2 = 3 * d
+    const left3 = 5 * d;
+
+    const dotStyles = { width: d, height: d, top: d };
+
+    return isLoading ? (
+        <div className={classes.overlay}>
+        <div className={classes.ldsEllipsis} style={{ height: 3 * d, width }}>
+          <div className={colorStyle} style={{ ...dotStyles, left: left1 }}></div>
+          <div className={colorStyle} style={{ ...dotStyles, left: left1 }}></div>
+          <div className={colorStyle} style={{ ...dotStyles, left: left2 }}></div>
+          <div className={colorStyle} style={{ ...dotStyles, left: left3 }}></div>
+        </div>
+      </div>
+    ) : null
+}
+
+export default Loader
