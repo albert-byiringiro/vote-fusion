7e20f988f0f80ee302d99ff95b4c85a3c1582fb3
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-09 10:59:58 +0200

    feature: added RankedCheckbox component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
+import RankedCheckBox from "./components/ui/RankedCheckBox";
+
 export default function App() {
   return (
     <>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <RankedCheckBox value={"choose the Lord to be happy."} rank={7}/>
     </>
   )
 }
