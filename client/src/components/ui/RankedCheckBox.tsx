14a3e90d7ec63a38c5421eb9003c084e3038e895
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-09 13:52:05 +0200

    refactor: added onSelect prop

--- client/src/components/ui/RankedCheckBox.tsx
+++ client/src/components/ui/RankedCheckBox.tsx
@@ -1,11 +1,11 @@
 type RankedCheckBoxProps = {
     rank?: number;
     value: string;
-    onSelect?: () => void;
+    onSelect: () => void;
 }
 
-const RankedCheckBox = ({ value, rank }: RankedCheckBoxProps) => (
-    <div className="my-4 box btn-orange relative">
+const RankedCheckBox = ({ value, rank, onSelect }: RankedCheckBoxProps) => (
+    <div className="my-4 box btn-orange relative" onClick={() => onSelect()}>
         <div className="">{value}</div>
         {rank && (
             <div className="absolute w-6 h-6 -top-3 -right-3 rounded-full bg-purple-600">
