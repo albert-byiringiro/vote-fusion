57a4f2ab0624a0634f0578191bb23987f88b795c
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-09 11:54:21 +0200

    feature: added a useEffect for snack bar prop

--- client/src/components/ui/SnackBar.tsx
+++ client/src/components/ui/SnackBar.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 type SnackBarProps = {
     type?: 'standard' | 'error';
     title?: string;
@@ -7,8 +9,35 @@ type SnackBarProps = {
     onClose: () => void;
 }
 
-const SnackBarStyles = {
+const snackBarStyles = {
     standard: 'bg-gray-100 bg-opacity-50',
     error: 'bg-red-600 text-white',
 }
 
+const SnackBar = ({
+    type = 'standard',
+    title,
+    message,
+    show,
+    onClose,
+    autoCloseDuration
+}: SnackBarProps) => {
+    const outerStyles = snackBarStyles[type]
+    const [showSnackBar, setShowSnackBar] = useState(false)
+
+    const handleCloseSnackBar = () => {
+        setShowSnackBar(false)
+    }
+
+    useEffect(() => {
+        if (show) {
+            setShowSnackBar(true)
+        }
+
+        const autoTimer = autoCloseDuration && setTimeout(() => handleCloseSnackBar(), autoCloseDuration)
+
+        return () => {
+            autoTimer && clearTimeout(autoTimer)
+        }
+    }, [show, message, title])
+}
