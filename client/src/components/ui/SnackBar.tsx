bf049dbaa7e131f58a1055a78739ba10dcedb477
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-09 11:40:16 +0200

    refactor: added snackbar styles

--- client/src/components/ui/SnackBar.tsx
+++ client/src/components/ui/SnackBar.tsx
@@ -6,3 +6,9 @@ type SnackBarProps = {
     autoCloseDuration?: number;
     onClose: () => void;
 }
+
+const SnackBarStyles = {
+    standard: 'bg-gray-100 bg-opacity-50',
+    error: 'bg-red-600 text-white',
+}
+
