0c9215da4c3393da0d11e941e0cf1acf96750ee4
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-10 15:27:28 +0200

    feature: added colorizeText util function

---
+++ client/src/utils.tsx
@@ -0,0 +1,11 @@
+export const colorizeText = (text: string): JSX.Element[] => text.split('').map((val, index) => {
+    return val.charCodeAt(0) >= 48 && val.charCodeAt(0) <= 57 ? (
+        <span key={index} className="text-orange-600">
+            {val}
+        </span>
+    ) : (
+        <span key={index} className="text-purple-600">
+            {val}
+        </span>
+    )
+});
