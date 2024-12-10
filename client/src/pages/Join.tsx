c7055f701097c667956be8b4f4223278ccea1036
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-10 09:33:23 +0200

    feature: added Join Page

--- client/src/pages/Join.tsx
+++ client/src/pages/Join.tsx
@@ -1,6 +1,6 @@
 const Join: React.FC = () => (
-    <div className="">
-        <h3 className="">Enter Code Provided by &quot;Friend&quot;</h3>
+    <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sm">
+        <h3 className="text-center">Enter Code Provided by &quot;Friend&quot;</h3>
     </div>
 )
 
