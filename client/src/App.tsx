1f59186d48e5142fa202b7214133e4740faba6e9
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-10 03:27:07 +0200

    feature: added Create component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,16 @@
 import React, { useState } from 'react';
 import ParticipantList from "./components/ParticipantList";
 import { Participants } from '../../shared/poll-types';
+import Create from './pages/Create';
 
 export default function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  
-  const participants: Participants = {
-    "1": "Alice",
-    "2": "Bob",
-    "3": "Charlie"
-  };
-
-  const handleRemoveParticipant = (id: string) => {
-    console.log(`Removing participant with ID: ${id}`);
-    // Add logic to remove participant
-  };
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <button onClick={() => setIsOpen(true)} className="mb-4">
-        Show Participants
-      </button>
-      <ParticipantList 
-        userID={""}
-        isAdmin={true}
-        onRemoveParticipant={handleRemoveParticipant}
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        participants={participants} children={undefined}      />
+      <Create/>
     </>
   );
 }
---
+++ client/src/pages/Create.tsx
@@ -0,0 +1,7 @@
+const Create: React.FC = () => (
+    <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sm">
+      <h3 className="text-center">Enter Poll Topic</h3>
+    </div>
+  );
+
+export default Create
