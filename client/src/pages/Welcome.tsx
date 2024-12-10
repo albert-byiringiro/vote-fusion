62e1a50fffe0921108934f9f7680509b32614367
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-10 08:56:05 +0200

    feature: added styles to welcome page

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -1,12 +1,14 @@
 const Welcome: React.FC = () => {
     return (
-        <div className="">
-            <h1 className="">Welcome to Rankr</h1>
-            <div className="">
-                <button className="">
+        <div className="flex flex-col justify-center items-center h-full">
+            <h1 className="text-center my-12">Welcome to Rankr</h1>
+            <div className="my-12 flex flex-col justify-center">
+                <button 
+                    className="box btn-orange my-2"
+                >
                     Create New Poll
                 </button>
-                <button className="">
+                <button className="box btn-purple my-2">
                     Joining Exisiting Poll
                 </button>
             </div>
