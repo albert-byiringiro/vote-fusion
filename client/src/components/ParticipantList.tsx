313cda161d134b4ed76cb1195a6d29194a0bac07
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-10 02:48:48 +0200

    refactor: added ParticipantList list return jsx.element

--- client/src/components/ParticipantList.tsx
+++ client/src/components/ParticipantList.tsx
@@ -1,47 +1,48 @@
-import { MdClose } from "react-icons/md";
-import { Participants } from "../../../shared/poll-types"
-import BottomSheet, { BottomSheetProps } from "./ui/BottomSheet";
+import React from 'react';
+import { MdClose } from 'react-icons/md';
+import BottomSheet, { BottomSheetProps } from './ui/BottomSheet';
+import { Participants } from '../../../shared/poll-types';
 
 type ParticipantListProps = {
-    participants?: Participants;
-    userID: string;
-    isAdmin: boolean;
-    onRemoveParticipant: (id: string) => void
-} & BottomSheetProps
+  participants?: Participants;
+  userID?: string;
+  isAdmin: boolean;
+  onRemoveParticipant: (id: string) => void;
+} & BottomSheetProps;
 
-const ParticipantList =  ({
-    isOpen,
-    onClose,
-    participants = {},
-    onRemoveParticipant,
-    userID,
-    isAdmin,
-}: ParticipantListProps) => {
-    <BottomSheet isOpen={isOpen} onClose={onClose}>
-        <div className="px-8 flex flex-wrap justify-center mb-2">
-            {Object.entries(participants).map(([id, participant]) => (
-                <div 
-                    key={id}
-                    className="mx-1 my-1 p-4 shadow-xl bg-white flex justify-between items-center rounded-md"
-                >
-                    <span className="ml-2 mr-1 text-indigo-700 text-xl text-center">
-                        {participant}
-                    </span>
-                    {isAdmin && userID !== id && (
-                        <span 
-                            className="ml-1 mr-2 cursor-pointer"
-                            onClick={() => onRemoveParticipant(id)}
-                        >
-                            <MdClose
-                                className="fill-current text-black align-middle"
-                                size={18}
-                            />
-                        </span>
-                    )}
-                </div>
-            ))}
+const ParticipantList: React.FC<ParticipantListProps> = ({
+  isOpen,
+  onClose,
+  participants = {},
+  onRemoveParticipant,
+  userID,
+  isAdmin,
+}): JSX.Element => (
+  <BottomSheet isOpen={isOpen} onClose={onClose}>
+    <div className="px-8 flex flex-wrap justify-center mb-2">
+      {Object.entries(participants).map(([id, participant]) => (
+        <div
+          key={id}
+          className="mx-1 my-1 p-4 shadow-xl bg-white flex justify-between items-center rounded-md"
+        >
+          <span className="ml-2 mr-1 text-indigo-700 text-xl text-center">
+            {participant}
+          </span>
+          {isAdmin && userID !== id && (
+            <span
+              className="ml-1 mr-2 cursor-pointer"
+              onClick={() => onRemoveParticipant(id)}
+            >
+              <MdClose
+                className="fill-current text-black align-middle"
+                size={18}
+              />
+            </span>
+          )}
         </div>
-    </BottomSheet>
-}
+      ))}
+    </div>
+  </BottomSheet>
+);
 
-export default ParticipantList
+export default ParticipantList;
