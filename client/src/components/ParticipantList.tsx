ae125c48eb0a37d8767deff833453c8965773cb1
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-10 02:42:55 +0200

    refactor: added participant list display

--- client/src/components/ParticipantList.tsx
+++ client/src/components/ParticipantList.tsx
@@ -1,3 +1,4 @@
+import { MdClose } from "react-icons/md";
 import { Participants } from "../../../shared/poll-types"
 import BottomSheet, { BottomSheetProps } from "./ui/BottomSheet";
 
@@ -15,4 +16,32 @@ const ParticipantList =  ({
     onRemoveParticipant,
     userID,
     isAdmin,
-}: ParticipantListProps) => {}
+}: ParticipantListProps) => {
+    <BottomSheet isOpen={isOpen} onClose={onClose}>
+        <div className="px-8 flex flex-wrap justify-center mb-2">
+            {Object.entries(participants).map(([id, participant]) => (
+                <div 
+                    key={id}
+                    className="mx-1 my-1 p-4 shadow-xl bg-white flex justify-between items-center rounded-md"
+                >
+                    <span className="ml-2 mr-1 text-indigo-700 text-xl text-center">
+                        {participant}
+                    </span>
+                    {isAdmin && userID !== id && (
+                        <span 
+                            className="ml-1 mr-2 cursor-pointer"
+                            onClick={() => onRemoveParticipant(id)}
+                        >
+                            <MdClose
+                                className="fill-current text-black align-middle"
+                                size={18}
+                            />
+                        </span>
+                    )}
+                </div>
+            ))}
+        </div>
+    </BottomSheet>
+}
+
+export default ParticipantList
