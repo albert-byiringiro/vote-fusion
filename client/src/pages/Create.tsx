a55902f916419c717499a1177696fd37bf24244e
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-13 18:38:02 +0200

    feature: added count selector component to the create poll

--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import CountSelector from "../components/ui/CountSelector"
 
 const Create: React.FC = () => {
   const [pollTopic, setPollTopic] = useState('')
@@ -11,6 +12,10 @@ const Create: React.FC = () => {
         <h3 className="text-center">Enter Poll Topic</h3>
         <div className="text-center w-full">
           <input type="text" className="box info w-full" maxLength={100} onChange={(e) => setPollTopic(e.target.value)} />
+        </div>
+        <h3 className="text-center mt-4 mb-2">Votes Per Participant</h3>
+        <div className="w-48 mx-auto my-4">
+          <CountSelector min={1} max={5} initial={3} step={1} onChange={(val) => setMaxVotes(val)}/>
         </div>
       </div>
     </div>
