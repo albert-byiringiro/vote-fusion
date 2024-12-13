260a55780a5e164eab2e035d5b6e0656466f50b1
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-13 18:30:19 +0200

    feature: added input for out topic

--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -7,7 +7,12 @@ const Create: React.FC = () => {
 
   return (
     <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sm">
-      <h3 className="text-center">Enter Poll Topic</h3>
+      <div className="mb-12">
+        <h3 className="text-center">Enter Poll Topic</h3>
+        <div className="">
+          <input type="text" className="" maxLength={100} />
+        </div>
+      </div>
     </div>
   )
 }
