81fcd883ab9cca54976119aea6dd56303f2883e2
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-13 18:27:10 +0200

    feature: added states for fields

--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -1,7 +1,15 @@
-const Create: React.FC = () => (
+import { useState } from "react"
+
+const Create: React.FC = () => {
+  const [pollTopic, setPollTopic] = useState('')
+  const [maxVotes, setMaxVotes] = useState(3)
+  const [name, setName] = useState('')
+
+  return (
     <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sm">
       <h3 className="text-center">Enter Poll Topic</h3>
     </div>
-  );
+  )
+}
 
 export default Create
