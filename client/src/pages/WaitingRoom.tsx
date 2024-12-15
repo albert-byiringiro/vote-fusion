e8ed88813fd68cffc28f6408b43ac3cfcb56def4
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-15 11:25:07 +0200

    feature: added waiting room use effect to initialize socket

--- client/src/pages/WaitingRoom.tsx
+++ client/src/pages/WaitingRoom.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react"
+import { actions } from "../state"
+
 export const WaitingRoom = () => {
+
+    useEffect(()=> {
+        console.log('Waiting room useEffect')
+        actions.initializeSocket()
+    }, [])
+
     return (
         <div className="flex flex-col w-full justify-between h-full">
             <h3>Waiting Room</h3>
