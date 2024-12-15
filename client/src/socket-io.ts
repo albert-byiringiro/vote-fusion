035cb7b2a35e93281e62452df7b9f97dfc72d85c
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-15 11:13:33 +0200

    feature: added createSocket handler function

--- client/src/socket-io.ts
+++ client/src/socket-io.ts
@@ -1,3 +1,4 @@
+import { io, Socket } from "socket.io-client";
 import { AppActions, AppState } from "./state";
 
 export const socketIOUrl =  `http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}/${import.meta.env.VITE_POLLS_NAMESPACE}`
@@ -6,4 +7,25 @@ type CreateSocketOptions = {
     socketIOUrl: string;
     state: AppState;
     actions: AppActions;
+}
+
+export const createSocketWithHandlers = ({
+    socketIOUrl,
+    state,
+    actions,
+}: CreateSocketOptions): Socket => {
+    console.log(`Creating socket with accessToken: ${state.accessToken}`)
+
+    const socket = io(socketIOUrl, {
+        auth: {
+            token: state.accessToken,
+        },
+        transports: ['websocket', 'polling'],
+    })
+
+    socket.on('connect', () => {
+        console.log(`Connected with socket ID: ${socket.id}. UserID: ${state.me?.id} will join room ${state.poll?.id}`)
+    })
+
+    return socket
 }
