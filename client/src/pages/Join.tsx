336df734090b4e0364a6701c99e0b5aa27e7073a
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-15 08:59:38 +0200

    feature: added join component states

--- client/src/pages/Join.tsx
+++ client/src/pages/Join.tsx
@@ -1,7 +1,15 @@
-const Join: React.FC = () => (
-    <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sm">
-        <h3 className="text-center">Enter Code Provided by &quot;Friend&quot;</h3>
-    </div>
-)
+import { useState } from "react"
+
+const Join: React.FC = () => {
+    const [pollID, setPollID] = useState('')
+    const [name, setName] = useState('')
+    const [apiError, setApiError] = useState('')
+    
+    return (
+        <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sm">
+            <h3 className="text-center">Enter Code Provided by &quot;Friend&quot;</h3>
+        </div>
+    )
+}
 
 export default Join
