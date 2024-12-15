958056d09d67df799071e8fbe1827f9dec68f2de
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-15 09:01:55 +0200

    feature: added a small validator for fields

--- client/src/pages/Join.tsx
+++ client/src/pages/Join.tsx
@@ -4,7 +4,19 @@ const Join: React.FC = () => {
     const [pollID, setPollID] = useState('')
     const [name, setName] = useState('')
     const [apiError, setApiError] = useState('')
-    
+
+    const areFieldsValid = (): boolean => {
+        if (pollID.length < 6 || pollID.length > 6) {
+            return false
+        }
+
+        if (name.length < 1 || name.length < 25) {
+            return false
+        }
+
+        return true
+    }
+
     return (
         <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sm">
             <h3 className="text-center">Enter Code Provided by &quot;Friend&quot;</h3>
