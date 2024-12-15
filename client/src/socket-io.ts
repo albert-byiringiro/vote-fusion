79fd46e3170b8aa8fec137458e7c1fdbf027556c
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-15 11:06:36 +0200

    feature: added create socket options

--- client/src/socket-io.ts
+++ client/src/socket-io.ts
@@ -1,9 +1,9 @@
-import { AppState } from "./state";
+import { AppActions, AppState } from "./state";
 
 export const socketIOUrl =  `http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}/${import.meta.env.VITE_POLLS_NAMESPACE}`
 
 type CreateSocketOptions = {
     socketIOUrl: string;
     state: AppState;
-    actions: 
+    actions: AppActions;
 }
