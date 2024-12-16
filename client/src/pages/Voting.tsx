cc27e21bf5690835c8a5f3c3d15cc9df840303a7
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-16 17:18:44 +0200

    feature: added states for the voting page and current state

--- client/src/pages/Voting.tsx
+++ client/src/pages/Voting.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react"
+import { useSnapshot } from "valtio"
+import { state } from "../state"
+
 export const Voting = () => {
+    const currentState = useSnapshot(state)
+    const [rankings, setRankings] = useState<string[]>()
+    const [confirmCancel, setConfirmCancel] = useState(false)
+    const [confirmVotes, setConfirmVotes] = useState(false)
+    
   return (
     <div>Voting</div>
   )
