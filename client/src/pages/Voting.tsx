30dee059b4b6608a204c628d2cdb1f2643ca4a7b
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-16 17:50:07 +0200

    feature: added total number of votes and votes remaining

--- client/src/pages/Voting.tsx
+++ client/src/pages/Voting.tsx
@@ -4,7 +4,7 @@ import { state } from "../state"
 
 export const Voting = () => {
     const currentState = useSnapshot(state)
-    const [rankings, setRankings] = useState<string[]>()
+    const [rankings, setRankings] = useState<string[]>([])
     const [confirmCancel, setConfirmCancel] = useState(false)
     const [confirmVotes, setConfirmVotes] = useState(false)
     
@@ -12,6 +12,18 @@ export const Voting = () => {
     <div className="mx-auto flex flex-col w-full justify-between items-center h-full max-w-sm">
         <div className="w-full">
             <h1 className="text-center">Voting Page</h1>
+        </div>
+        <div className="w-full">
+          {currentState.poll && (
+            <>
+              <div className="text-center text-xl font-semibold mb-6">
+                Select Your Top {currentState.poll?.votesPerVoter} choices
+              </div>
+              <div className="text-center text-lg font-semibold mb-6 text-indigo-700">
+                {currentState.poll.votesPerVoter - rankings?.length}
+              </div>
+            </>
+          )}
         </div>
     </div>
   )
