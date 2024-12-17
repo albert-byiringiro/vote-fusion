faeb41ffab9489a40f0d512bf12ba633cf87fe25
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-17 16:46:30 +0200

    feature: added the UI for the Admin to close the poll.

--- client/src/pages/Results.tsx
+++ client/src/pages/Results.tsx
@@ -1,10 +1,11 @@
 import { useSnapshot } from "valtio"
 import { state } from "../state"
 import { useState } from "react"
+import ResultCard from "../components/ui/ResultCard"
 
 export const Results = () => {
 
-    const { poll, isAdmin, participantCount } = useSnapshot(state)
+    const { poll, isAdmin, participantCount, rankingsCount } = useSnapshot(state)
     const [isConfirmationOpen, setIsConfirmationOpen] = useState(false)
     const [isLeavePollOpen, setIsLeavePollOpen] = useState(false)
 
@@ -12,8 +13,39 @@ export const Results = () => {
         <>
             <div className="mx-auto flex flex-col w-full justify-between items-center h-full max-w-sm">
                 <div className="w-full">
-                    <h1 className="text-center">Results</h1>
+                    <h1 className="text-center mt-12 mb-4">Results</h1>
+                    {
+                        poll?.results.length ? (
+                            <ResultCard results={poll?.results}/>
+                        ) : (
+                            <p className="text-center text-xl">
+                                <span className="text-orange-600">{rankingsCount}</span> 
+                                of {' '}
+                                <span className="text-purple-600">{participantCount}</span> participants have voted
+                            </p>
+                        )
+                    }
                 </div> 
+                <div className="flex flex-col justify-center">
+                    {
+                        isAdmin && !poll?.results.length && (
+                            <>
+                                <button className="box btn-orange my-2" onClick={() => setIsConfirmationOpen(true)}>End Poll</button>
+                            </>
+                        )
+                    }
+                    {
+                        isAdmin && poll?.results.length && (
+                            <div className="my-2 italic">
+                                Waiting for Admin, {' '}
+                                <span className="font-semibold">
+                                    {poll?.participants[poll?.adminID]}
+                                </span>
+                                , to finalize the poll.
+                            </div>
+                        )
+                    }
+                </div>
             </div>
         </>
     )
