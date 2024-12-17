bfe7d1a03dd257045decc6600edecc4a4e4b79f1
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-17 13:21:03 +0200

    feature: added results card component

--- client/src/components/ui/ResultCard.tsx
+++ client/src/components/ui/ResultCard.tsx
@@ -2,9 +2,9 @@ import { Results } from "../../../../shared/poll-types"
 
 type ResultCard = {
     results: DeepReadonly<Results>;
-}
+};
 
-const ResultCard: React.FC<ResultCard> = ({ results }) => {
+const ResultCard = ({ results }: ResultCard) => {
     return (
         <>
             <div className="grid grid-cols-3 gap-4 pb-2 my-2 border-b-2 border-solid border-purple-70 pr-4">
