58f73465f74ef6e3c34c1899b729fa19bef905d4
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-18 20:24:12 +0200

    added a vite-env.d.ts

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+declare module '*.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
