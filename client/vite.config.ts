07ed7c4997543e20469e7d81f37f55a7665c3435
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-12-18 13:35:51 +0200

    feature: optimized the vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,5 +10,20 @@ export default defineConfig({
     modules: {
       localsConvention: 'camelCaseOnly',
     },
+  },
+  build: {
+    minify: 'terser',
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utils: ['valtio', 'socket.io-client'],
+        },
+      },
+    },
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
   },
 });
